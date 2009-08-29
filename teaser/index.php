96c9c0c40a4104e16f501246e79f57d57b39c3ef
krema <krema@3445ca58-e459-48c6-80b6-aacbbe7ab71b>
2009-08-29 02:07:57 +0000

    created the start files for the teaser
    git-svn-id: https://krema.svn.beanstalkapp.com/hastuzeit@18 3445ca58-e459-48c6-80b6-aacbbe7ab71b

---
+++ teaser/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head profile="http://gmpg.org/xfn/11">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-Language" content="en,de"/>
+
+<title>My cool Template</title>
+
+<meta name="author" content="Matthias Kretschmann" />
+<meta name="copyright" content="&copy; 2008 Matthias Kretschmann" />
+<meta name="description" content="xxx" />
+<meta name="keywords" content="xxx" />
+
+<meta name="siteinfo" content="robots.txt" />
+<meta name="robots" content="follow,noimageindex"/>
+
+<!-- All the style sheets -->
+<link href="style/main.css" rel="stylesheet" media="screen, projection" type="text/css" />
+<!–[if IE ]>
+<link href="ie.css" rel="stylesheet" media="screen,projection" type="text/css" />
+<![endif]–>
+
+<!-- All the scripts -->
+<script type="text/javascript" src="js/effects.js"></script>
+
+<!-- Favicon -->
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+<link rel="icon" href="/favicon.ico" type="image/x-icon" />
+
+</head>
+
+<body>
+
+<div id="wrapper"><!-- optionally, you can also use just the body tag -->
+<div id="header">
+
+<!-- List Based Horizontal Navigation -->
+<ul id="nav">
+<li><a href="#">Home</a></li>
+<li class="current"><a href="#">About</a></li>
+<li><a href="#">Contact</a></li>
+<li><a href="#">Blog</a></li>
+<li><a href="#">News</a></li>
+</ul><!-- end List Based Horizontal Navigation -->
+
+</div><!-- end header -->
+
+<div id="content">
+	
+</div><!-- end content -->
+
+<div id="footer">
+	<p>&copy; 2008 All Rights Reserved<br />
+		Design- and Code-Voodoo by Matthias Kretschmann	
+	</p>
+</div><!-- end footer -->
+
+</div><!-- end wrapper -->
+</body>
+</html>
