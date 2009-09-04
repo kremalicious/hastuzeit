88d9f9ae594cd7e43dfc06f2a7d813681e1be31e
krema <krema@3445ca58-e459-48c6-80b6-aacbbe7ab71b>
2009-09-04 16:59:15 +0000

    addressed some validation errors. We're now perfectly XHTML 1.1 valid! Yay!
    git-svn-id: https://krema.svn.beanstalkapp.com/hastuzeit@47 3445ca58-e459-48c6-80b6-aacbbe7ab71b

--- teaser/index.php
+++ teaser/index.php
@@ -15,9 +15,9 @@
 
 <!-- All the style sheets -->
 <link href="style/main.css" rel="stylesheet" media="screen, projection" type="text/css" />
-<!–[if IE ]>
+<!-- [if IE ]-->
 <link href="ie.css" rel="stylesheet" media="screen,projection" type="text/css" />
-<![endif]–>
+<!-- [endif]-->
 
 <!-- Favicon and apple-touch icon-->
 <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
