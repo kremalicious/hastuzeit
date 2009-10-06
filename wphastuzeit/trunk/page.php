06b0a298560b48fb431bcacc66d64ed21973a49f
krema <krema@3445ca58-e459-48c6-80b6-aacbbe7ab71b>
2009-10-06 05:59:24 +0000

    - wp-caption - changed edit link color
    git-svn-id: https://krema.svn.beanstalkapp.com/hastuzeit@82 3445ca58-e459-48c6-80b6-aacbbe7ab71b

--- wphastuzeit/trunk/page.php
+++ wphastuzeit/trunk/page.php
@@ -11,7 +11,7 @@ get_header(); ?>
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<div class="post" id="post-<?php the_ID(); ?>">
 			<h1><?php the_title(); ?></h1>
-			<?php edit_post_link('Edit', '<p>', '</p>'); ?>
+			<?php edit_post_link('Bearbeiten', '<p>', '</p>'); ?>
 			<?php the_content('<p>Read the rest of this page &raquo;</p>'); ?>
 			<?php wp_link_pages(array('before' => '<p>Pages: ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
 		</div>
