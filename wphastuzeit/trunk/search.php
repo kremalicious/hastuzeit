683948a09278c442dd1f8c990b59cf98aec48fcf
krema <krema@3445ca58-e459-48c6-80b6-aacbbe7ab71b>
2009-10-07 05:13:51 +0000

    refined search results
    git-svn-id: https://krema.svn.beanstalkapp.com/hastuzeit@94 3445ca58-e459-48c6-80b6-aacbbe7ab71b

--- wphastuzeit/trunk/search.php
+++ wphastuzeit/trunk/search.php
@@ -33,9 +33,11 @@
 
 <?php get_header(); ?>
 
+	<div id="main">
+
 	<?php if (have_posts()) : ?>
 
-		<h2>Search Results</h2>
+		<h1 class="pagetitle">Suchergebnisse</h1>
 
 
 		<?php while (have_posts()) : the_post(); ?>
@@ -43,30 +45,28 @@
 			<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
 						
 					<p class="meta"><span class="date"><?php the_time('M Y') ?></span> <span class="category-link"><?php the_category(' ') ?></span> <?php edit_post_link('Bearbeiten', '', ''); ?> <span class="alignright comment-link"><?php comments_popup_link('0', '1', '%'); ?></span></p>
+					
+					<div class="alignleft image">
+						<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php if (function_exists('images')) images('1', '100', '100', '', false); ?></a>
+					</div>
+					
+					<div class="alignleft text">
 		
-					<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-					
-					<!-- Wenn custom field unterueberschrift vorhanden, dann anzeigen -->
-					<?php 
-						$unterueber = get_post_meta($post->ID, "Unterüberschrift", true);
-		      			if ($unterueber != "")
-		          			echo "<h2 class=\"unterueber\">$unterueber</h2>";
-					?>
-					
-					<p class="author">von <?php if(function_exists('coauthors_posts_links'))
-							    coauthors_posts_links(", ", " und ");
-							else
-							    the_author_posts_link(); ?></p>
+						<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+						
+						<!-- Wenn custom field unterueberschrift vorhanden, dann anzeigen -->
+						<?php 
+							$unterueber = get_post_meta($post->ID, "Unterüberschrift", true);
+			      			if ($unterueber != "")
+			          			echo "<h2 class=\"unterueber\">$unterueber</h2>";
+						?>
+						
+						<p class="author">von <?php if(function_exists('coauthors_posts_links'))
+								    coauthors_posts_links(", ", " und ");
+								else
+								    the_author_posts_link(); ?></p>
+					</div>
 							
-					<!-- Nutze exzerpt, wenn angegeben, ansonsten the_content -->
-					<?php if (!empty($post->post_excerpt)) : ?>
-						<?php if (function_exists('images')) images('1', '150', '150', '', false); ?>
-						<?php the_excerpt(); ?>
-						<p><a class="more-link" href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>">Mehr, mehr, mehr</a></p>
-					<?php else : ?>
-						<?php the_content('Mehr, mehr, mehr'); ?>
-					<?php endif; ?>
-					
 				</div>
 
 		<?php endwhile; ?>
