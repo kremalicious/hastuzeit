be10ecd0f978b8a7193c6d891e4e263097e2f342
krema <krema@3445ca58-e459-48c6-80b6-aacbbe7ab71b>
2009-10-09 01:19:40 +0000

    - The Featured Slider, Yay! -> This works by using the sticky posts feature - switched all the custom queries on the startpage -> pagination works correctly, sticky and heftarchiv posts won't show up in the main article listing
    git-svn-id: https://krema.svn.beanstalkapp.com/hastuzeit@110 3445ca58-e459-48c6-80b6-aacbbe7ab71b

--- wphastuzeit/trunk/index.php
+++ wphastuzeit/trunk/index.php
@@ -6,54 +6,100 @@
 
 get_header(); ?>
 
-			<?php if ( is_home() ) { ?>
+			<?php if ( is_home() and !is_paged() ) { ?>
+			
 				<div id="featured">
-				
-					<!-- The Featured Loop -->
 					
-					 <?php $featured_query = new WP_Query('category_name=Featured&showposts=1');
-					  while ($featured_query->have_posts()) : $featured_query->the_post();
-					  $do_not_duplicate[] = $post->ID ?>
-					  
-					  <div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-					  
-					  	<p class="meta"><span class="date"><?php the_time('M Y') ?></span> <span class="category-link"><?php echo the_category_exclude(" ","featured"); ?></span> <?php edit_post_link('Bearbeiten', '', ''); ?> <span class="alignright comment-link"><?php comments_popup_link('0', '1', '%'); ?></span></p>
-
-							<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Link zu <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>	
-							<!-- wenn custom field unterueberschrift vorhanden, dann anzeigen -->
-							<?php 
-								$unterueber = get_post_meta($post->ID, "Unterüberschrift", true);
-				      			if ($unterueber != "")
-				          			echo "<h2 class=\"unterueber\">$unterueber</h2>";
-							?>
+					<div id="featured_head">
+						<h3>Featured</h3>
+					</div>
+					
+					<div class="scroll">
+					<div class="scrollContainer">
+				
+						<!-- The Featured Loop -->
+						<?php
+						 $args = array(
+							'showposts' => 9,
+							'post__in'  => get_option('sticky_posts'),
+							'caller_get_posts' => 1
+						);
+						 $postslist = get_posts($args);
+						 foreach ($postslist as $post) : 
+						    setup_postdata($post);
+						 ?>
+						
+						<div class="panel post hentry" id="<?php the_title_attribute(); ?>">
+						
+							<div class="featured_media">
+						
+								<a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title(); ?>"><?php images('1', '150', '150', 'alignleft', false); ?></a>
 							
-							<p class="author-link">von <?php if(function_exists('coauthors_posts_links'))
-							    coauthors_posts_links(", ", " und ");
-							else
-							    the_author_posts_link(); ?></p>
-										
-							<!-- Nutze exzerpt, wenn angegeben, ansonsten the_content -->
-							<?php if (!empty($post->post_excerpt)) : ?>
-								<?php the_excerpt(); ?>
-								<p><a class="more-link" href="<?php the_permalink(); ?>" rel="bookmark" title="Link zu <?php the_title(); ?>">Read the rest of this entry &raquo;</a></p>
-							<?php else : ?>
-								<?php the_content('Mehr, mehr, mehr'); ?>
-							<?php endif; ?>
+							</div>
 							
-						</div>	  
-					    
-					  <?php endwhile; ?>
+							<div class="featured_text">
+								
+								<h4><a href="<?php the_permalink() ?>" rel="bookmark" title="Link zu <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h4>
+								<!-- Wenn custom field unterueberschrift vorhanden, dann anzeigen -->
+								<?php 
+									$unterueber = get_post_meta($post->ID, "Unterüberschrift", true);
+					      			if ($unterueber != "")
+					          			echo "<h5 class=\"unterueber\">$unterueber</h5>";
+								?>
+								
+								
+								<a class="more-link" href="<?php the_permalink(); ?>" title="Link zu <?php the_title_attribute(); ?>">Mehr, Mehr, Mehr</a>
+							
+							</div>
+	
+						</div> <!– End Panel –>
+						
+						<?php endforeach; ?>
+					
+					</div><!-- end .scrollContainer -->
+					</div><!-- end .scroll -->
+					
+					<!-- Here comes the featured navigation -->
+					<div id="shade">
+					
+						<ul class="featured_nav">
+							
+							<?php
+							 $args = array(
+								'showposts' => 9,
+								'post__in'  => get_option('sticky_posts'),
+								'caller_get_posts' => 1
+							);
+							 $postslist = get_posts($args);
+							 foreach ($postslist as $post) : 
+							    setup_postdata($post);
+							 ?>
+							
+							<li class="thumb">
+								<a href="#<?php the_title_attribute(); ?>" title="<?php the_title_attribute(); ?>"><?php images('1', '50', '50', 'alignleft', false); ?></a>
+							</li>
+							
+							<?php endforeach; ?>
+							
+						
+						</ul>
+						
+					</div>
+					
+					
 					
 					<div id="zipfel"></div>
 				</div>
 				
 				<div id="ausgabe">
-				
-					<!-- The Paper Loop -->
+					 
 					
-					<?php $paper_query = new WP_Query('category_name=Heftarchiv&showposts=1');
-					  while ($paper_query->have_posts()) : $paper_query->the_post();
-					  $do_not_duplicate[] = $post->ID ?>
+					<!-- The Paper Loop -->
+						<?php
+						 $postslist = get_posts('category_name=Heftarchiv&showposts=1');
+						 foreach ($postslist as $post) : 
+						    setup_postdata($post);
+						 ?>
 					  
 					  	<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
 					  	
@@ -63,63 +109,65 @@ get_header(); ?>
 							
 						</div>
 					  
-					  <?php endwhile; ?>
-				
+					  <?php endforeach; ?>
+
 				</div>
 			<?php } ?>
 			
 			<div id="main">
 				
 				<!-- The Main Loop -->
-
-				<?php if (have_posts()) : while (have_posts()) : the_post(); 
-				 if (in_array($post->ID, $do_not_duplicate)) continue;
-				 update_post_caches($posts); ?>
 				
-						<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-						
-							<p class="meta"><span class="date"><?php the_time('M Y') ?></span> <span class="category-link"><?php the_category(' ') ?></span> <?php edit_post_link('Bearbeiten', '', ''); ?> <span class="alignright comment-link"><?php comments_popup_link('0', '1', '%'); ?></span></p>
+				<?php
+				$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+				$sticky=get_option('sticky_posts');
+				$args=array(
+				   'cat'=>-5,
+				   'caller_get_posts'=>1,
+				   'post__not_in' => $sticky,
+				   'paged'=>$paged,
+				   );
+				$postslist = get_posts($args);
+						 foreach ($postslist as $post) : 
+						    setup_postdata($post);
+				?>
 				
-							<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Link zu <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-							
-							<!-- Wenn custom field unterueberschrift vorhanden, dann anzeigen -->
-							<?php 
-								$unterueber = get_post_meta($post->ID, "Unterüberschrift", true);
-				      			if ($unterueber != "")
-				          			echo "<h2 class=\"unterueber\">$unterueber</h2>";
-							?>
-							
-							<p class="author-link">von <?php if(function_exists('coauthors_posts_links'))
-							    coauthors_posts_links(", ", " und ");
-							else
-							    the_author_posts_link(); ?></p>
-									
-							<!-- Nutze exzerpt, wenn angegeben, ansonsten the_content -->
-							<?php if (!empty($post->post_excerpt)) : ?>
-								<?php if (function_exists('images')) images('1', '150', '150', '', false); ?>
-								<?php the_excerpt(); ?>
-								<p><a class="more-link" href="<?php the_permalink(); ?>" rel="bookmark" title="Link zu <?php the_title(); ?>">Mehr, mehr, mehr</a></p>
-							<?php else : ?>
-								<?php the_content('Mehr, mehr, mehr'); ?>
-							<?php endif; ?>
-							
-						</div>
-				
-					<?php endwhile; ?>
+					<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
 					
+						<p class="meta"><span class="date"><?php the_time('M Y') ?></span> <span class="category-link"><?php the_category(' ') ?></span> <?php edit_post_link('Bearbeiten', '', ''); ?> <span class="alignright comment-link"><?php comments_popup_link('0', '1', '%'); ?></span></p>
+			
+						<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Link zu <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+						
+						<!-- Wenn custom field unterueberschrift vorhanden, dann anzeigen -->
+						<?php 
+							$unterueber = get_post_meta($post->ID, "Unterüberschrift", true);
+			      			if ($unterueber != "")
+			          			echo "<h2 class=\"unterueber\">$unterueber</h2>";
+						?>
+						
+						<p class="author-link">von <?php if(function_exists('coauthors_posts_links'))
+						    coauthors_posts_links(", ", " und ");
+						else
+						    the_author_posts_link(); ?></p>
+								
+						<!-- Nutze exzerpt, wenn angegeben, ansonsten the_content -->
+						<?php if (!empty($post->post_excerpt)) : ?>
+							<?php if (function_exists('images')) images('1', '150', '150', '', false); ?>
+							<?php the_excerpt(); ?>
+							<p><a class="more-link" href="<?php the_permalink(); ?>" rel="bookmark" title="Link zu <?php the_title(); ?>">Mehr, mehr, mehr</a></p>
+						<?php else : ?>
+							<?php the_content('Mehr, mehr, mehr'); ?>
+						<?php endif; ?>
+						
+					</div>
+					
+				<?php endforeach; ?>
+									
 					<div class="next-links">
 						<div class="alignleft"><?php next_posts_link('&laquo; &Auml;ltere Artikel') ?></div>
 						<div class="alignright"><?php previous_posts_link('Neuere Artikel &raquo;') ?></div>
 					</div>
-				
-				
-				<?php else : ?>
-				
-					<h2>Not Found</h2>
-					<p>Sorry, but you are looking for something that isn't here.</p>
-					<?php get_search_form(); ?>
-				
-				<?php endif; ?>
+
 
 <?php get_sidebar(); ?>
 
--- wphastuzeit/trunk/style/js/effects.js
+++ wphastuzeit/trunk/style/js/effects.js
@@ -13,6 +13,88 @@ $(function () { //DOMdiDOM
 	//AJAX Live Search
     $('#s').liveSearch({url: '/index.php?ajax=1&s='});
     
+    
+    //The Featured Slider, omnomnom
+    
+    if ( $("#featured").length > 0 ) {
+	    var $panels = $('#featured .scrollContainer .panel');
+		var $container = $('#featured .scrollContainer');
+		var horizontal = true;
+		if (horizontal) {
+	        $panels.css({
+	            'float' : 'left',
+	            'position' : 'relative'
+	        });
+	        $container.css('width', $panels[0].offsetWidth * $panels.length);
+	    }
+		
+		var $scroll = $('#featured .scroll').css('overflow', 'hidden');
+		
+		function selectNav() {
+		  $(this)
+		    .parents('ul:first')
+		      .find('a')
+		        .removeClass('selected')
+		      .end()
+		    .end()
+		    .addClass('selected');
+		}
+		
+		$('#featured .featured_nav').find('a').click(selectNav);
+		
+		function trigger(data) {
+		  var el = $('#featured .featured_nav').find('a[href$="' + data.id + '"]').get(0);
+		  selectNav.call(el);
+		}
+		
+		if (window.location.hash) {
+		  trigger({ id : window.location.hash.substr(1) });
+		} else {
+		  $('.featured_nav a:first').click();
+		}
+	
+		var offset = parseInt((horizontal ? 
+		  $container.css('paddingTop') : 
+		  $container.css('paddingLeft')) 
+		  || 0) * -1;
+		
+		var scrollOptions = {
+		  target: $scroll,
+		  items: $panels,
+		  navigation: '.featured_nav a',
+		  axis: 'xy',
+		  onAfter: trigger,
+		  offset: offset,
+		  duration: 500,
+		};
+		
+		// apply serialScroll to the slider - we chose this plugin because it 
+		// supports// the indexed next and previous scroll along with hooking 
+		// in to our navigation.
+		$('#featured').serialScroll(scrollOptions);
+	
+		$.localScroll(scrollOptions);
+	
+		scrollOptions.duration = 1;
+		$.localScroll.hash(scrollOptions);
+		
+		// start to automatically cycle the tabs
+		var cycleTimer = setInterval(function () {
+		   $scroll.trigger('next');
+		}, 5000);
+		
+		var $stopTriggers = $('#slider .navigation').find('a')
+		    .add('.scroll')
+		    .add("a[href^='#']");
+		
+		function stopCycle() {
+		   $stopTriggers.unbind('click.cycle');
+		   clearInterval(cycleTimer);
+		}
+		$stopTriggers.bind('click.cycle', stopCycle);
+	}
+
+    
     //Search term highlighting init
 	if(typeof(hls_query) != 'undefined'){
       $('#content').highlight(hls_query, 1, 'hls');
@@ -31,7 +113,7 @@ $(function () { //DOMdiDOM
 $(window).load(function() {
 	
 	//qTip
-	$('#content .hentry a[title]').qtip({
+	$('#main .hentry a[title]').qtip({
 		show: {
       		delay: 20,
       		effect: {
@@ -52,6 +134,38 @@ $(window).load(function() {
       		background: 'transparent',
 		    tip: { // Now an object instead of a string
 	        	corner: 'topLeft', // We declare our corner within the object using the corner sub-option
+	        	color: '#ccc'
+	        },
+		    border: {
+	        	width: 1,
+	        	color: '#ccc'
+	      	},
+	      
+		}
+	});
+	
+	//qTip Featured
+	$('#featured .featured_nav a[title]').qtip({
+		show: {
+      		delay: 10,
+      		effect: {
+      			type: 'slide', 
+      			length: 100 } 
+      	},
+		position: {
+      		corner: { 
+      			target: 'rightTop', 
+      			tooltip: 'topLeft'
+      		}
+      	},
+      	style: {
+      		color: '#666',
+      		lineHeight: '1.2em',
+      		fontFamily: '"Trebuchet MS", "Lucida Grande", Lucida, Verdana, sans-serif',
+      		maxWidth: 200,
+      		background: 'transparent',
+		    tip: { 
+	        	corner: 'leftMiddle',
 	        	color: '#ccc'
 	        },
 		    border: {
