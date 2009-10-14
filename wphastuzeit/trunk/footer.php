914f13e089c397e1fedf9797136152cd6528fc5a
krema <krema@3445ca58-e459-48c6-80b6-aacbbe7ab71b>
2009-10-14 18:32:55 +0000

    removed google analytics code from the theme, will be added by plugin
    git-svn-id: https://krema.svn.beanstalkapp.com/hastuzeit@170 3445ca58-e459-48c6-80b6-aacbbe7ab71b

--- wphastuzeit/trunk/footer.php
+++ wphastuzeit/trunk/footer.php
@@ -34,16 +34,6 @@
 		<script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/style/js/effects.js"></script>
 
 		<?php wp_footer(); ?>
-		
-		<script type="text/javascript">
-		var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-		document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-		</script>
-		<script type="text/javascript">
-		try {
-		var pageTracker = _gat._getTracker("UA-10899823-1");
-		pageTracker._trackPageview();
-		} catch(err) {}</script>
 
 	</body>
 
