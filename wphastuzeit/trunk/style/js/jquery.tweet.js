e75876cad7190e75b6ec21ba238dc36dc55aa069
krema <krema@3445ca58-e459-48c6-80b6-aacbbe7ab71b>
2010-01-20 06:21:47 +0000

    tweet display refinements
    git-svn-id: https://krema.svn.beanstalkapp.com/hastuzeit@306 3445ca58-e459-48c6-80b6-aacbbe7ab71b

--- wphastuzeit/trunk/style/js/jquery.tweet.js
+++ wphastuzeit/trunk/style/js/jquery.tweet.js
@@ -127,7 +127,7 @@
           var join = ((s.join_text) ? join_template : ' ')
           var avatar_template = '<a class="tweet_avatar" href="http://twitter.com/'+ item.from_user+'"><img src="'+item.profile_image_url+'" height="'+s.avatar_size+'" width="'+s.avatar_size+'" alt="'+item.from_user+'\'s avatar" border="0"/></a>';
           var avatar = (s.avatar_size ? avatar_template : '')
-          var date = '<a class="tweetdate" href="http://twitter.com/'+item.from_user+'/statuses/'+item.id+'" title="view tweet on twitter">'+relative_time(item.created_at)+' |</a><a class="tweetdate" href ="http://twitter.com/'+item.from_user+'">@'+item.from_user+'</a>';
+          var date = '<em class="tweetdate"><a href="http://twitter.com/'+item.from_user+'/statuses/'+item.id+'" title="view tweet on twitter">'+relative_time(item.created_at)+' |</a><a href ="http://twitter.com/'+item.from_user+'">@'+item.from_user+'</a></em>';
           var text = '<span class="tweet_text">' +$([item.text]).linkUrl().linkUser().linkHash().makeHeart().capAwesome().capEpic()[0]+ '</span>';
           
           // until we create a template option, arrange the items below to alter a tweet's display.
