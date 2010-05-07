04c8a19b8c378259572751ca1b7179d42ef4349e
krema <krema@3445ca58-e459-48c6-80b6-aacbbe7ab71b>
2010-05-07 15:47:52 +0000

    added facebook like button under each article
    git-svn-id: https://krema.svn.beanstalkapp.com/hastuzeit@335 3445ca58-e459-48c6-80b6-aacbbe7ab71b

--- wphastuzeit/trunk/single.php
+++ wphastuzeit/trunk/single.php
@@ -36,6 +36,8 @@ get_header();
 			
 			?>
 			
+			<iframe src="http://www.facebook.com/plugins/like.php?href=<?php echo urlencode(get_permalink($post->ID)); ?>&amp;layout=standard&amp;show-faces=true&amp;width=500&amp;action=like&amp;colorscheme=light" scrolling="no" frameborder="0" allowTransparency="true" style="border:none; overflow:hidden; width:580px; height:40px"></iframe>
+			
 			<?php wp_link_pages(array('before' => '<div class="page-link"><p><strong>Seiten:</strong> ', 'after' => '</p></div>', 'next_or_number' => 'number')); ?>
 			
 			<?php if ( !in_category('Termine') ) autorbox(); ?>
@@ -47,6 +49,7 @@ get_header();
 				<p id="shortlink"><?php if (function_exists("twitter_link")) { echo 'Kurz-URL: <a href="' . twitter_link() . '">'.twitter_link().'</a>'; } ?></p>
 				
 			</div>
+			
 			
 			
 		</div>
