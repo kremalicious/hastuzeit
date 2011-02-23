e3a4a3bcaea87a53465a21828a26887f81d97549
krema <krema@3445ca58-e459-48c6-80b6-aacbbe7ab71b>
2011-02-23 01:35:03 +0000

    Nr 35 Header Color, 2011 Archive
    git-svn-id: https://krema.svn.beanstalkapp.com/hastuzeit@351 3445ca58-e459-48c6-80b6-aacbbe7ab71b

--- wphastuzeit/trunk/page-heftarchiv.php
+++ wphastuzeit/trunk/page-heftarchiv.php
@@ -17,6 +17,45 @@ Template Name: Heftarchiv
 				<?php the_content(); ?>
 					  
 				<?php endwhile; endif; ?>
+				
+				<h2 class="accordionButton">2011</h2>
+				
+				<div class="accordionContent">
+				
+					<!-- Hefte 2011 Loop -->
+					<?php
+						$postslist = query_posts('category_name=Heftarchiv&showposts=-1&year=2011');
+					 	foreach ($postslist as $post) : 
+					    setup_postdata($post);
+					?>
+					
+						<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
+					
+							<h3><?php the_title(); ?></h3>
+							
+							<a class="alignleft" href="<?php $values = get_post_custom_values("Cover big"); echo $values[0]; ?>" title="Cover hastuzeit <?php the_title(); ?>">		
+								<img src="<?php $values = get_post_custom_values("Cover small"); echo $values[0]; ?>" alt="Cover <?php the_title(); ?>" class="thumb" width="100" height="139" />
+							</a>
+
+							<?php 
+								foreach((get_the_category()) as $childcat) {
+									if (cat_is_ancestor_of(31, $childcat)) { ?>
+										<h4><a class="read" href="<?php echo get_category_link($childcat->term_id);?>">Texte lesen (<?php echo $childcat->category_count;?>)</a></h4>
+								<?php } } 
+								
+								$pdf = 'Download';
+								$themeta = get_post_meta($post->ID, $pdf, TRUE);
+								
+								if($themeta != '') {
+									echo '<h4><a href="'. $themeta .'">Download (pdf)</a></h4>';
+								}
+								
+								?>
+						</div>
+						
+					<?php endforeach; ?>
+				
+				</div>
 								
 				<h2 class="accordionButton">2010</h2>
 				
